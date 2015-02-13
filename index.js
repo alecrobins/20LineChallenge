a2b91707f1824a74eeb18a56227252c18ce875dc
Alec Robins <alecrobins@gmail.com>
2015-02-13 19:54:14 +0800

    21 lines , one more to go. . .

--- index.js
+++ index.js
@@ -1,138 +1,127 @@
 // 20 line challenge
+var iL=document.getElementById('i'), c=document.getElementById('c'), fD = {},
+x=c.getContext('2d'), cP=0, cA=0; iL.addEventListener('change', hI, !1);
+// Read in the image data
+function hI(e){var r = new FileReader(); r.onload = function(event){
+	var img = new Image(); img.onload = function(){
+	  c.width = img.width; c.height = img.height; x.drawImage(img,0,0); }
+		img.src = event.target.result; }; r.readAsDataURL(e.target.files[0]); }
+// Generate the moasic
+function imgD () { var iD = x.getImageData(0,0,c.width,c.height); getData();
+$("#MI").css({ "width": c.width*20 }); setTimeout(function(){
+for(var i = 0; i < iD.data.length; i += 4){ var rgb = "0px 20px rgba(" +
+   iD.data[i] + "," + iD.data[i + 1] + "," + iD.data[i + 2] + ", 0.8) inset";
+	var uq = fD[cA].id.toString() + parseInt(Math.random()*9999999999).toString();
+	var imgSrc =  "url(https://farm" + fD[cA].farm + ".staticflickr.com/" + fD[cA].server +
+	"/" + fD[cA].id + "_" + fD[cA].secret+ "_s.jpg)"; $("#MI").append("<div id='" + uq + "' class='it' ></div>");
+// Assign a tint for moasic pictures //XXX: this could be replaced by tineye
+$(("#" + uq)).css({"position": "relative", "display": "block", "float": "left",
+"width": "20px", "height": "20px", "background-size": "cover", "box-shadow": rgb,
+"background": imgSrc}); cA++; if(cA == 500){cA = 0;} } }, 5000); }
+// Get Flickr data
+function getData() { $.getJSON("https://api.flickr.com/services/rest/?&method=f"
++"lickr.photos.search&api_key=33f27236670b6107249908b17b9dfb58&tags=dog%2Cfamil"
++"y%2Clandscape%2Ccat%2Ccandid%2Ccity%2Csmile%2Chappy%2Clove%2Cjoy%2Cbeach%2Cmo"
++"untains&tag_mode=any&content_type=1&per_page=500&page"+cP+"&format=json&jsonc"
++"allback=?", function(d){ fD = d.photos.photo; }); }
 
-var imageLoader = document.getElementById('imageLoader');
-		imageLoader.addEventListener('change', handleImage, false);
-var canvas = document.getElementById('imageCanvas');
-var ctx = canvas.getContext('2d');
+//
+// function imgD () {
+// 	var iD = x.getImageData(0,0,c.width,c.height);
+// 	getData(); $("#MI").css({ "width": c.width*20 });
+// 	setTimeout(function(){
+// 		for(var i = 0; i < iD.data.length; i += 4){
+//
+// 				// if( ((i/4) % 1 )== 0 ){
+// 					//getImages( "0px 20px rgba(" + iD.data[i] + "," + iD.data[i + 1] + "," + iD.data[i + 2] + ", 0.8) inset");
+// 					var rgb = "0px 20px rgba(" + iD.data[i] + "," + iD.data[i + 1] + "," + iD.data[i + 2] + ", 0.8) inset";
+// 					var uq = fD[cA].id.toString();
+// 					uq = uq + parseInt(Math.random()*999999999).toString();
+// 					var cid = "#" + uq;
+// 					var imgSrc =  "url(https://farm" + fD[cA].farm + ".staticflickr.com/" + fD[cA].server +
+// 							"/" + fD[cA].id + "_" + fD[cA].secret+ "_s.jpg)";
+//
+// 					console.log(i);
+//
+// 					if( (i/4) % c.width == 0){
+// 						imgSrc = "cyan";
+// 						rgb = "none";
+// 					}
+//
+// 					// $("#MI").append("<div id='" + fD[cA].id + "' class='it' ><img src=' https://farm" + fD[cA].farm + ".staticflickr.com/" + fD[cA].server +
+// 					// "/" + fD[cA].id + "_" + fD[cA].secret+ "_s.jpg' /></div>");
+//
+// 					$("#MI").append("<div id='" + uq + "' class='it' ></div>")
+//
+// 					$(cid).css({
+// 						"position": "relative",
+// 						"display": "block",
+// 						"float": "left",
+// 						"width": "20px",
+// 						"height": "20px",
+// 						"background-size": "cover",
+// 						"box-shadow": rgb,
+// 						"background": imgSrc
+// 					});
+//
+// 					cA++;
+//
+// 					if(cA == 500){
+// 						cP++;
+// 						console.log("RESET");
+// 						cA = 0;
+// 						//getData();
+// 					}
+//
+// 					// console.log(d);
+// 				// }
+//
+// 		}
+//
+// 	}, 5000);
+//
+//
+//
+// }
+//
+//
+// function getData() {
+// 	var userID = "39233913@N02";
+// 	var apiKey = "33f27236670b6107249908b17b9dfb58";
+// 	var itemsPerPage = 500; // Max is 500
+//
+// 	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + apiKey + "&tags=dog%2Cfamily%2Clandscape%2Ccat%2Ccandid%2Ccity%2Csmile%2Chappy%2Clove%2Cjoy%2Cbeach%2Cmountains&tag_mode=any&content_type=1&per_page=" + itemsPerPage + "&page" + cP + "&format=json&jsoncallback=?", function(data){
+// 			console.log("DATA LOADED");
+// 			fD = data.photos.photo;
+// 	});
+// }
 
-var curPage = 0;
-var curAtt = 0;
-var flickrData = {};
-
-function handleImage(e){var reader = new FileReader(); reader.onload =
-	function(event){
-				var img = new Image();
-				img.onload = function(){
-						canvas.width = img.width;
-						canvas.height = img.height;
-						ctx.drawImage(img,0,0);
-				}
-				img.src = event.target.result;
-		}
-		reader.readAsDataURL(e.target.files[0]);
-
-}
-
-function imgD () {
-	var c=document.getElementById("imageCanvas");
-	var ctx=c.getContext("2d");
-
-	var imgData=ctx.getImageData(0,0,c.width,c.height);
-
-	//NOTE: pixes are stores in [r1,g1,b1,a1,r2,g2,b2,a2,r,g....]
-
-	getData();
-
-	$("#newImage").css({
-		"width": c.width*10,
-
-	});
-
-	console.log(imgData);
-
-	setTimeout(function(){
-
-		for(var i = 0; i < imgData.data.length; i += 4){
-
-				// if( ((i/4) % 1 )== 0 ){
-					//getImages( "0px 20px rgba(" + imgData.data[i] + "," + imgData.data[i + 1] + "," + imgData.data[i + 2] + ", 0.8) inset");
-					var rgb = "0px 10px rgba(" + imgData.data[i] + "," + imgData.data[i + 1] + "," + imgData.data[i + 2] + ", 0.8) inset";
-					var uq = flickrData[curAtt].id.toString();
-					uq = uq + parseInt(Math.random()*999999999).toString();
-					var cid = "#" + uq;
-					var imgSrc =  "url(https://farm" + flickrData[curAtt].farm + ".staticflickr.com/" + flickrData[curAtt].server +
-							"/" + flickrData[curAtt].id + "_" + flickrData[curAtt].secret+ "_s.jpg)";
-
-					console.log(i);
-
-					if( (i/4) % c.width == 0){
-						imgSrc = "cyan";
-						rgb = "none";
-					}
-
-					// $("#newImage").append("<div id='" + flickrData[curAtt].id + "' class='it' ><img src=' https://farm" + flickrData[curAtt].farm + ".staticflickr.com/" + flickrData[curAtt].server +
-					// "/" + flickrData[curAtt].id + "_" + flickrData[curAtt].secret+ "_s.jpg' /></div>");
-
-					$("#newImage").append("<div id='" + uq + "' class='it' ></div>")
-
-					$(cid).css({
-						"position": "relative",
-						"display": "block",
-						"float": "left",
-						"width": "10px",
-						"height": "10px",
-						"background-size": "cover",
-						"box-shadow": rgb,
-						"background": imgSrc
-					});
-
-					curAtt++;
-
-					if(curAtt == 500){
-						curPage++;
-						console.log("RESET");
-						curAtt = 0;
-						//getData();
-					}
-
-					// console.log(d);
-				// }
-
-		}
-
-	}, 5000);
-
-
-
-}
-
-function getImages (rgb) {
-
-	var uq = flickrData[curAtt].id + parseInt(Math.random()*1000);
-	var cid = "#" + uq;
-	var imgSrc =  "url(https://farm" + flickrData[curAtt].farm + ".staticflickr.com/" + flickrData[curAtt].server +
-	 		"/" + flickrData[curAtt].id + "_" + flickrData[curAtt].secret+ "_s.jpg)";
-
-	console.log(curAtt);
-
-	// $("#newImage").append("<div id='" + flickrData[curAtt].id + "' class='it' ><img src=' https://farm" + flickrData[curAtt].farm + ".staticflickr.com/" + flickrData[curAtt].server +
-	// "/" + flickrData[curAtt].id + "_" + flickrData[curAtt].secret+ "_s.jpg' /></div>");
-
-	$("#newImage").append("<div id='" + uq + "' class='it' ></div>")
-
-	$(cid).css({
-		"position": "relative",
-		"display": "block",
-		"float": "left",
-		"width": "10px",
-		"height": "10px",
-		"background-size": "cover",
-		"box-shadow": rgb,
-		"background": imgSrc
-	});
-
-	console.log("IMAGE: ");
-	console.log(imgSrc);
-
-}
-
-function getData() {
-	var userID = "39233913@N02";
-	var apiKey = "33f27236670b6107249908b17b9dfb58";
-	var itemsPerPage = 500; // Max is 500
-
-	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + apiKey + "&tags=dog%2Cfamily%2Clandscape%2Ccat%2Ccandid%2Ccity%2Csmile%2Chappy%2Clove%2Cjoy%2Cbeach%2Cmountains&tag_mode=any&content_type=1&per_page=" + itemsPerPage + "&page" + curPage + "&format=json&jsoncallback=?", function(data){
-			console.log("DATA LOADED");
-			flickrData = data.photos.photo;
-	});
-}
+// function getImages (rgb) {
+//
+// 	var uq = fD[cA].id + parseInt(Math.random()*1000);
+// 	var cid = "#" + uq;
+// 	var imgSrc =  "url(https://farm" + fD[cA].farm + ".staticflickr.com/" + fD[cA].server +
+// 			"/" + fD[cA].id + "_" + fD[cA].secret+ "_s.jpg)";
+//
+// 	console.log(cA);
+//
+// 	// $("#MI").append("<div id='" + fD[cA].id + "' class='it' ><img src=' https://farm" + fD[cA].farm + ".staticflickr.com/" + fD[cA].server +
+// 	// "/" + fD[cA].id + "_" + fD[cA].secret+ "_s.jpg' /></div>");
+//
+// 	$("#MI").append("<div id='" + uq + "' class='it' ></div>")
+//
+// 	$(cid).css({
+// 		"position": "relative",
+// 		"display": "block",
+// 		"float": "left",
+// 		"width": "10px",
+// 		"height": "10px",
+// 		"background-size": "cover",
+// 		"box-shadow": rgb,
+// 		"background": imgSrc
+// 	});
+//
+// 	console.log("IMAGE: ");
+// 	console.log(imgSrc);
+//
+// }
